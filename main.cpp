364f53eb73d3b006f99f7761f51b1774f5c43a52
Julia <julenka_p@mail.ru>
2024-06-11 14:38:15 +0300

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -253,7 +253,7 @@ int main() {
     So if I wanna delete the 3rd, 5th, and 7th elements of the list, I have to delete the 2,4,6-i elements.
     However, I cant do it all at once because the .erase() function can only delete one element at a time. 
     Therefore I will need to call this function 3 times, but each time I call it, the list will be reindexed.
-    The 1st index I need to delete is still 2, but the subsequent indexes will be shifted by 1.
+    The 1st index I need to delete is still 2, but the subsequent indexes will be shifted by the number of deleted elements.
     As a result, I have to delete one by one 2,3,4-i elements of the list.
     */
     Array_Container.erase(2);
